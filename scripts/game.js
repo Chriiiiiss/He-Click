1f7c0da30322351f9c92c4cb40c8981855d3924b
Alexis Kessab <alexis.kessab@hetic.net>
2020-04-21 18:02:29 +0200

    Drag and Drop beginning

--- scripts/game.js
+++ scripts/game.js
@@ -35,3 +35,73 @@ leave2.addEventListener( 'click', () =>{
     elementCharacter.style.display = 'inline'
     elementHetic.style.display = 'inline'
 })
+
+
+
+
+//                 DRAG AND DROP GESTION
+
+const fill = document.querySelector('.fill')
+const emptiesInv = document.querySelectorAll('.inv')
+const emptiesIco = document.querySelectorAll('.ico')
+const empties = Array.from(emptiesInv).concat(Array.from(emptiesIco))
+
+const typeItem = fill.classList[1]
+
+
+// Fill Listener
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empties and call drag events
+for (const empty of empties){
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
+
+//Drag Function
+function dragStart(){
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd(){
+    this.className = 'fill hand';
+    
+}
+
+function dragOver(){
+    event.preventDefault();
+}
+
+function dragEnter(){
+    event.preventDefault();
+}
+
+function dragLeave(){
+    this.className = 'inv'
+}
+
+function dragDrop(){
+    const invItem = this.classList[0]
+    if ((typeItem == 'hand' && invItem == 'inv-hand')
+    || (typeItem =='head' && invItem =='inv-head' )
+    || (typeItem == 'body' && invItem == 'inv-head')
+    || (typeItem == 'foot' && invItem == 'inv-foot')
+    || invItem == 'inv'){
+        this.classname = 'invItem inv-full'
+        this.append(fill)
+    }
+}
+
+//PROBLEEME : Ne met pas la class "inv-full" ?
+
+
+// function dragDrop(){
+//     this.className = 'inv-full'
+//     this.append(fill)
+// }
