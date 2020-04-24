6a0eceb6fd119895e04d4eee4f074debfb135a8d
Chriiiiiss <chris.fraanc@gmail.com>
2020-04-24 04:34:33 +0200

    [ADD] sound system merged to game

--- scripts/game.js
+++ scripts/game.js
@@ -1175,7 +1175,19 @@ const lvlUp = () =>
     dmgSpan.textContent = `${actualDmg}`
     magSpan.textContent = `${actualMag}`
     powerSpan.textContent = `${actualPower}%`
-    }
+}
+
+function play(idPlayer) {
+    const player = document.querySelector('#' + idPlayer);
+    
+        player.play();
+}
+
+function resume(idPlayer) {
+    const player = document.querySelector('#' + idPlayer);
+	
+    player.pause();
+}
 
 
 
